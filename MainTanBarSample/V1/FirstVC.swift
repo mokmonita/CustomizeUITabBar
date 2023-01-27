ee0a2f857dfa95b74c9b28bcbce6c729f0fe6206
Monita <>
2023-01-27 17:32:52 +0700

    first commit

---
+++ MainTanBarSample/V1/FirstVC.swift
@@ -0,0 +1,17 @@
+//
+//  FirstVC.swift
+//  MainTanBarSample
+//
+//  Created by Mok Monita on 27/1/23.
+//
+
+import UIKit
+
+class FirstVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+}
---
+++ MainTanBarSample/V3/ThirdVC.swift
@@ -0,0 +1,29 @@
+//
+//  ThirdVC.swift
+//  MainTanBarSample
+//
+//  Created by Mok Monita on 27/1/23.
+//
+
+import UIKit
+
+class ThirdVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
